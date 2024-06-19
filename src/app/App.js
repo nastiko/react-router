d2ca64c04aaf140b45a4ea1323ccc35dc546b735
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-06-19 21:55:51 +0100

    started practising the "react-router"

---
+++ src/app/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Category from "../components/Category";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+import EditProfileForm from "../components/EditProfileForm";
+import Root from "../components/Root";
+
+import { Route, RouterProvider } from 'react-router-dom';
+
+import "./App.css";
+
+const router = 
+  /* Wrap this Root Route to create Router here */
+  <Route path="/" element={ <Root/> }>
+    {/* Add Routes here! */}
+  </Route>
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
